45ed0b0e18aa7b1169f97e8ce74651a315f9bb6c
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-03-23 19:17:42 +0300

    add Dark mode

--- src/app/components/DarkModeSwitch.jsx
+++ src/app/components/DarkModeSwitch.jsx
@@ -23,16 +23,18 @@ Finally, the "DarkModeSwitch" component is exported as the default export of the
 import { MdLightMode } from "react-icons/md";
 import { BsFillMoonFill } from "react-icons/bs";
 import { useTheme } from "next-themes";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
+
 function DarkModeSwitch() {
   const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   useEffect (()=> {
-    
-  })
+    setMounted(true)
+  },[])
   const currentTheme = theme === "system" ? systemTheme : theme;
   return (
     <>
-      {currentTheme === "dark" ? (
+      { mounted &&  (currentTheme === "dark" ? (
         <MdLightMode
           className="text-xl cursor-pointer hover:text-amber-500"
           onClick={() => setTheme("light")}
@@ -42,7 +44,7 @@ function DarkModeSwitch() {
           className="text-xl cursor-pointer hover:text-amber-500"
           onClick={() => setTheme("dark")}
         />
-      )}
+      ))}
     </>
   );
 }
