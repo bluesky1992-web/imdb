fab6ebc1e37258e8e461ab68614ad210b8dcee69
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-03-25 20:05:39 +0300

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl  font-medium  text-amber-600">About</h1>
+      <p>
+        The movie website created by our team is a comprehensive platform for
+        all movie enthusiasts. It features a user-friendly interface that allows
+        users to easily navigate through the website and find the latest movie
+        releases, upcoming movies, and movie reviews.
+      </p>
+      <p>
+        Our team has curated a vast collection of movies from various genres and
+        languages, making it a one-stop-shop for all movie lovers. The website
+        is regularly updated with the latest movie news and trailers, so users
+        can stay up-to-date with all the happenings in the movie world.
+      </p>
+
+      <p>
+        Additionally, the website also features a rating system that allows
+        users to rate and review movies, helping other users to make informed
+        decisions about which movies to watch. Overall, the movie website
+        created by our team is a comprehensive and engaging platform that caters
+        to the needs of all movie enthusiasts.
+      </p>
+    </div>
+  );
+}
