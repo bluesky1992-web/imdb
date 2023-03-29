15193e9ba1c199643cab5ca854f49daa436673ee
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-03-29 20:05:59 +0300

    fetch data from tmdb  api in the home page

---
+++ src/app/components/Results.jsx
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+
+export default function Results({ results }) {
+  return (
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>
+          {result.original_title}
+        </div>
+      ))}
+    </div>
+  );
+}
